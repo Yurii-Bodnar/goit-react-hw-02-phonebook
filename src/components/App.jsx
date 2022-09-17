9271202592bd6a8e0b228b5bbfa41c463324c577
Yurii-Bodnar <yuriy.bodnar75@gmail.com>
2022-09-17 17:31:16 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,17 +11,15 @@ export class App extends Component {
       {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter: '',
-    //PhonebookForm
     name: '',
     number: ''
   }
   
-// PhoneboookForm
   handleChange = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
-//PhonebookForm
+
   addContacts = (contacts, name, number) => {
     this.setState(prewState =>{
       return{
@@ -30,36 +28,34 @@ export class App extends Component {
     })
     
   };
-//PhonebookForm
   reset = () => {
     this.setState({name: '' , number: ''});
   };
-//PhonebookForm
+
   handleSubmitt = e => {
     e.preventDefault();
     this.addContacts(this.state.contacts,this.state.name,this.state.number);
     this.reset();
   };
-// лишається в апп
+
   eventFilterContacts = e =>{ 
     this.setState({ filter: e.target.value });
     
   }
-//лишається в апп
+
   getVisibleContacts= () =>{
     const normalizedFilter = this.state.filter.toLowerCase();
     return this.state.contacts.filter(contact =>(contact.name.toLowerCase().includes(normalizedFilter)))
   }
 
   render() {
-    //забрати name,number
+ 
     const {  name, number, filter } = this.state;
     const visibleContact = this.getVisibleContacts()
 
     return (
       <>
       <h1>Phonebook</h1>
-      //handleChange,handleSubmitt,name,number, переносим в компонент 
         <PhonebookForm
           handleChange={this.handleChange}
           handleSubmitt={this.handleSubmitt}
@@ -67,7 +63,7 @@ export class App extends Component {
           number={number}
         />
         <h2>Contacts</h2>
-        //visibleContact, і функ getVisibleContacts, value,filterContacts , лишається в  App, number забираєм
+       
         <Contacts contacts={visibleContact} number={number} value={filter} filterContacts={this.eventFilterContacts} />
       </>
     );
